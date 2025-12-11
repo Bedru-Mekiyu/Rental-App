4b2d9f28a714c3ab716a70ceaeccba9bb426c49c
Suad Abrar <suadabrar44@gmail.com>
2025-12-11 17:31:09 +0300

    improve MongoDB connection handling and logging

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,28 @@
-// src/config/db.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const MONGODB_URI =
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/rms';
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/rms";
+
+  if (!MONGODB_URI) {
+    console.error("MONGODB_URI missed in environment variables");
+    process.exit(1)
+  }
 
   try {
     await mongoose.connect(MONGODB_URI);
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
+
+    mongoose.connection.on("error", (err) => {
+      console.error("MongoDB connection error:", err);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.warn("MongoDB diconnected");
+    });
   } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1); // stop app if DB fails
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
   }
 };
 
